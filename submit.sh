2d14c9339cfc5a562babdf7f798c747b65d63513
Matej <matouhrin@gmail.com>
2020-07-10 09:47:38 +0200

    submit.sh script removed

--- submit.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-export TIME_LIMIT=2000
-export MEMORY_LIMIT=5048576
-dmoj-cli -c config.yml -- submit footballinteractive PY3 /problems/solutions/football_interactive.py -tl $TIME_LIMIT -ml $MEMORY_LIMIT
-
-#dmoj-cli -c config.yml -- submit football PY3 submissions/football.py -tl 2000 -ml 5048576
